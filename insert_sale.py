5d5dc9e3238f1340f3736bf0455468c5c153bae5
Maria <msilvest@nd.edu>
2022-11-02 20:52:50 -0400

    Now We Can Insert Sales and Update Merch Quantity

--- insert_sale.py
+++ insert_sale.py
@@ -14,17 +14,22 @@ def return_all():
                                 database='msilvest')
     with connection:
         with connection.cursor() as cursor:
-            Sale_ID = 1
-            Item_ID = 111001
-            Size = 'S'
-            Price = 30.00
+            New_Sale_ID = 1
+            Sale_Item_ID = 111001
+            Sale_Size = 'S'
+            Sale_Price = 30.00
             Quantity_Sold = 1
 
-            # Insert new record
-            sql = f"INSERT INTO Sale_Trial (Sale_ID, Item_ID, Size, Price, Quantity_Sold) VALUES ('{Sale_ID}', '{Item_ID}', '{Size}', '{Price}', '{Quantity_Sold}')"
+            # Insert new sale
+            sql_sale = f"INSERT INTO Sale_Trial (Sale_ID, Item_ID, Size, Price, Quantity_Sold) VALUES ('{New_Sale_ID}', '{Sale_Item_ID}', '{Sale_Size}', '{Sale_Price}', '{Quantity_Sold}')"
             # cursor.execute("INSERT INTO Sale_Trial (Sale_ID, Item_ID, Size, Price, Quantity_Sold) VALUES (1, 111001, 'S', 30.00, 1)");
-            cursor.execute(sql); 
-        
+            cursor.execute(sql_sale); 
+       
+            # Update Merch Quantity
+            sql_quantity = f"UPDATE Merch_Trial SET Quantity = Quantity - '{Quantity_Sold}' WHERE Item_ID = '{Sale_Item_ID}' AND Size = '{Sale_Size}'"
+            
+            cursor.execute(sql_quantity);
+
         connection.commit()
 
     response_body = {
